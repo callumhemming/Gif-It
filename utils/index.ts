7cc53b145cf2b0294a6e711bbcd3327e73084783
callumhemming <callum.m.hemming@outlook.com>
2022-04-26 19:13:36 +0100

    Added config system

---
+++ utils/index.ts
@@ -0,0 +1,27 @@
+import Config from "../config";
+
+
+const {outputFolder, filePrefix} = Config()
+
+
+function getPreviousNumbers():string[]{
+
+  return [""]
+}
+
+
+/**
+ * Returns a random string of numerial characters.
+ * The length of the string is based on param1: len:number
+*@param len Length of random string
+*@returns String of random characters
+*/
+export function getRandomString(len: number): string {
+  function getRandomNumber(): string {
+    return String(Math.floor(Math.random() * 9));
+  }
+  const randomCharList = Array(len)
+    .fill("")
+    .map(v => getRandomNumber());
+  return randomCharList.join("");
+}
