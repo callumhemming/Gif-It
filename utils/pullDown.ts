60cc4dcc3e81a29cdeb450900913739577a9b8ac
callumhemming <callum.m.hemming@outlook.com>
2022-04-27 19:25:27 +0100

    Commiting for reasons

--- utils/pullDown.ts
+++ utils/pullDown.ts
@@ -1,11 +1,9 @@
 import Shell from "shelljs"
 
 export default async function pullDown(hash:string){
-    console.log(`git checkout ${hash};
+    
+    Shell.exec(`git checkout ${hash};
     git pull;
-    npm i;`)
-    // Shell.exec(`git checkout ${hash};
-    // git pull;
-    // npm i;
-    // `)
+    npm i;
+    `)
 }
