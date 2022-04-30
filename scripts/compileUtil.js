8947a0bca0f70d44df7d66d63b71c610615c64c5
callumhemming <callum.m.hemming@outlook.com>
2022-04-30 20:57:43 +0100

    Clearing changes...

--- scripts/compileUtil.js
+++ scripts/compileUtil.js
@@ -1,13 +1,16 @@
-
-const fs = require("fs")
 const Shell = require("shelljs")
+const fs = require("fs")
+
+
+
+
 
 
 function incrementVersion(){
     const readPackageJSON = JSON.parse(fs.readFileSync('package.json', 'utf8'))
     const versionArray = readPackageJSON.version.split(".")
     versionArray[2] = String(Number(versionArray[2])+1)
-
+    
     return versionArray.join(".")
 }
 
@@ -19,3 +22,19 @@ if(fs.existsSync("../GifitTestSite/GifitOutput")){Shell.exec("rm -rf ../GifitTes
 Shell.exec('git commit -am "Clearing changes..."')
 Shell.exec(`npm version ${incrementVersion()}`)
 Shell.exec("npm pack -pack-destination ../GifitTestSite/bins")
+
+// const {exec} = require("child_process")
+
+
+// process.chdir("../GifitTestSite/Test")
+// let startServer = exec(`npm run dev`)
+// Shell.exec("echo hello")
+// startServer.kill()
+// startServer = exec(`npm run dev`)
+// Shell.exec("git checkout 0805d222c06cf4a4dab9646a8ca4d75b6cb38639")
+// startServer.kill()
+// Shell.exec("git checkout main")
+// startServer = exec(`npm run dev`)
+// startServer.kill()
+
+// startServer.kill()
--- utils/takeScreenshot.ts
+++ utils/takeScreenshot.ts
@@ -1,7 +1,7 @@
 import Shell from "shelljs";
 import puppeteer from "puppeteer";
 import getRandomString from "./getRandomString";
-
+import {exec} from "child_process"
 
 interface ScreenshotArgs{
     path:string;
@@ -14,14 +14,17 @@ export default async function takeScreenshot(
   commit
 ): Promise<any> {
 
+
+  const startServer = exec("npm run dev")
+  
   
   await Shell.exec(`git checkout ${commit.hash}`)
   
 
 
 
-  console.log(process.cwd())
-    const {path, outputFolder, filePrefix} = args
+  
+  const {path, outputFolder, filePrefix} = args
 
   const browser = await puppeteer.launch({
     // headless: false,
@@ -31,11 +34,34 @@ export default async function takeScreenshot(
   const page = await browser.newPage();
 
   await page.goto(path);
+
+  await page.reload({ waitUntil: ["networkidle0", "domcontentloaded"] });
   await page.screenshot({
     path: `../${outputFolder}/${filePrefix}${getRandomString(5)}.png`,
   });
 
 
+  startServer.kill()
   await Shell.exec(`git checkout main`)
   return await browser.close();
 }
+
+
+
+// const {exec} = require("child_process")
+
+
+// // const fs = require("fs")
+// const Shell = require("shelljs")
+
+
+// process.chdir("../GifitTestSite/Test")
+// let startServer = exec(`npm run dev`)
+// Shell.exec("echo hello")
+// startServer.kill()
+// startServer = exec(`npm run dev`)
+// Shell.exec("git checkout 0805d222c06cf4a4dab9646a8ca4d75b6cb38639")
+// startServer.kill()
+// Shell.exec("git checkout main")
+// startServer = exec(`npm run dev`)
+// startServer.kill()
